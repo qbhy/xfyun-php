9dd8522b764c27bb53fca92290970891f11a9a35
qbhy <qbhy0715@qq.com>
2021-09-26 10:46:47 +0800

    feat: 修改测试用例

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,7 +2,10 @@
 
 require_once __DIR__ . '/../vendor/autoload.php';
 
-$repository = Dotenv\Repository\RepositoryBuilder::createWithDefaultAdapters()
+$repository = Dotenv\Repository\RepositoryBuilder::createWithNoAdapters()
+    ->addAdapter(Dotenv\Repository\Adapter\EnvConstAdapter::class)
+    ->addWriter(Dotenv\Repository\Adapter\PutenvAdapter::class)
+    ->immutable()
     ->make();
 
 $dotenv = Dotenv\Dotenv::create($repository, __DIR__ . '/..');
